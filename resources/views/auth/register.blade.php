cb58f33c2fa0120d7cdf77e124e867f171680683
Rudrika15 <rudrikadave20226@gmail.com>
2024-05-23 10:15:40 +0530

    design register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,19 +10,55 @@
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+    {{-- <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/influencer.css') }}" rel="stylesheet" /> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
+    {{-- 
     <!-- toastr -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css"> 
+    --}}
+
+
+    {{-- new  --}}
+
+    {{-- <link rel="icon" href="{{ asset('assetshtml/images/fav_icon.png" type="image/x-icon') }}"> --}}
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('assetshtml/css/custom.css') }}" />
+
+    <!-- Box Icons CSS -->
+    <link href="{{ asset('assetshtml/css/boxicons.min.css') }}" rel='stylesheet'>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('assetshtml/css/bootstrap.css') }}">
+
+    <!-- Owl CSS -->
+    <link rel="stylesheet" href="{{ asset('assetshtml/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assetshtml/css/owl.theme.default.min.css') }}">
+
+    <!-- Brand Story Slider CSS -->
+    <link rel="stylesheet" href="{{ asset('assetshtml/css/swiper-bundle.min.css') }}">
+
+    <!-- WOW Animate CSS -->
+    <link rel="stylesheet" href="{{ asset('assetshtml/css/animate.css') }}">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
 </head>
 
+
+
 <body>
-    <div class="d-flex justify-content-center pt-3 ">
-        <!-- Sidebar-->
-        <div class="border p-5" style="width: 30%">
-            <div class="text-info text-center pb-2 fw-bold">Brand beans</div>
+
+    @include('extra.homePageMenu')
+
+    <div class="container" style="margin-top: 150px; max-width: 500px; margin-bottom: 100px">
+        <div class="card ">
+
+
+
+            {{-- <div class="text-info text-center pb-4 fw-bold"><img src="{{ asset('images/logo.png') }}" class="w-25" alt=""></div> --}}
             <form action="{{ route('register') }}" method="post">
                 @csrf
 
@@ -35,7 +71,7 @@
                     @enderror
                 </div>
 
-
+                <br>
                 <div class="mb-3">
                     <input type="email" class="form-control" placeholder="Email" value="{{ old('email') }}" id="email" name="email">
                     @error('email')
@@ -44,7 +80,7 @@
                         </span>
                     @enderror
                 </div>
-
+                <br>
                 <div class="mb-3">
                     <input type="text" class="form-control" placeholder="Create Your Unique Username" value="{{ old('username') }}" id="username" name="username">
                     @error('username')
@@ -53,7 +89,7 @@
                         </span>
                     @enderror
                 </div>
-
+                <br>
                 <?php
                     session_start();
                     if (isset($_SESSION['mobileno'])) {
@@ -67,6 +103,7 @@
                         </span>
                     @enderror
                 </div>
+                <br>
                 <?php
                     } else {
                         $mobileno = "";
@@ -78,6 +115,7 @@
                         </span>
                     @enderror
                 </div>
+                <br>
                 <?php
                     }
                     ?>
@@ -89,39 +127,15 @@
                         </span>
                     @enderror
                 </div>
+                <br>
                 <div class="mb-3"><input type="password" class="form-control" placeholder="Confirm Password" class="" id="password_confirmation" name="confirm-password"></div>
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <label><input type="radio" class="" id="" name="type" value="Individual">Individual</label>
-                        <label><input type="radio" class="" id="" name="type" value="Business">Business</label>
-                    </div>
-                </div>
-
                 <br>
-                <div class="mb-3 Business box" style="display: none;">
-                    <select name="category" id="category" class="form-control">
-                        <option selected disabled>Select Category</option>
-                        @foreach ($categories as $data)
-                            <option value="{{ $data->id }}">{{ $data->name }}</option>
-                        @endforeach
-                        <option value="other">Other</option>
-                    </select>
-                    @error('category')
-                        <span role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
 
 
-                <div class="mb-3" id="other" style="display: none;">
-                    <input type="text" class="form-control" placeholder="Add Other Category" name="categoryname" class="">
-                </div>
-
-                <div class="mb-3">
+                {{-- <div class="mb-3">
                     <input type="text" class="form-control" placeholder="Do you have Refer Code??" class="" id="refer" name="refer">
-                </div>
+                </div> --}}
 
 
 
@@ -129,38 +143,25 @@
 
 
             </form>
+            <hr>
+            <div class="pt-2" style="display: flex; justify-content: space-between">
+                <a href="{{ route('otp.login') }}" class="a-link"><i class="fa fa-user-plus"></i>Login with OTP</a>
+                <a href="{{ route('register') }}" class="a-link"><i class="fa fa-user-plus"></i>Register</a>
+            </div>
         </div>
+
     </div>
 
+
+
+
+    @include('extra.homePageFooter')
     <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
     <script src="{{ asset('js/scripts.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 
-    <script>
-        $(document).ready(function() {
-            $('#category').on('change', function() {
-                if (this.value == 'other') {
-                    $("#other").show();
-                } else {
-                    $("#other").hide();
-                }
-            });
-        });
-    </script>
-
-    <!-- user type hide and show code -->
-    <script>
-        $(document).ready(function() {
-            $('input[type="radio"]').click(function() {
-                var inputValue = $(this).attr("value");
-                var targetBox = $("." + inputValue);
-                $(".box").not(targetBox).hide();
-                $(targetBox).show();
-            });
-        });
-    </script>
 
 </body>
 
